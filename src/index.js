145e211626b799962faf154b3fdfa6a7e888d6f6
rilangki <rilangkishabong@gmail.com>
2020-10-31 16:38:22 +0530

    Used signin and signup routes inside switch

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import { App } from "./App";
+import SignIn from "./SignIn";
+import { SignUp } from "./SignUp";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Switch>
+        <Route path="/signin" component={SignIn} />
+        <Route path="/signup" component={SignUp} />
+        <Redirect to="/signup" />
+      </Switch>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
