425a0be641a95a581fb9b7786457ea7154b92ad2
rilangki <rilangkishabong@gmail.com>
2020-11-13 17:53:33 +0530

    created sidebar component.

---
+++ src/TemporaryDrawer.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { withRouter } from "react-router-dom";
+import { Home as HomeIcon } from "@material-ui/icons";
+import PeopleIcon from "@material-ui/icons/People";
+import {
+  Drawer,
+  ListItem,
+  Typography,
+  List,
+  Box,
+  Divider,
+  ListItemIcon,
+} from "@material-ui/core";
+
+const TemporaryDrawer = ({ opened, toggleDrawer, history }) => {
+  return (
+    <Drawer anchor="left" open={opened} onClose={toggleDrawer}>
+      <List>
+        <ListItem>
+          <Box>
+            <Typography variant="h4">Clan-Relations</Typography>
+          </Box>
+        </ListItem>
+
+        <Divider />
+        <ListItem>
+          <ListItemIcon
+            button
+            onClick={() => {
+              toggleDrawer();
+              history.push("/home");
+            }}
+          >
+            <HomeIcon />
+            <Typography> Home</Typography>
+          </ListItemIcon>
+        </ListItem>
+        <Divider />
+
+        <ListItem>
+          <ListItemIcon
+            button
+            onClick={() => {
+              toggleDrawer();
+              history.push("/clan/hello");
+            }}
+          >
+            <PeopleIcon />
+            <Typography> Show Clans</Typography>
+          </ListItemIcon>
+        </ListItem>
+      </List>
+    </Drawer>
+  );
+};
+
+export const SideBarWithRouter = withRouter(TemporaryDrawer);
