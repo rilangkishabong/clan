72df542ce4817cc9aeadbe5bebc2f28f53448b8a
rilangki <rilangkishabong@gmail.com>
2020-11-13 17:54:38 +0530

    configured sidebar with the navbar.

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   AppBar,
   Button,
@@ -13,50 +13,60 @@ import {
   ExitToApp as LogoutIcon,
   AddCircle as AddClanIcon,
 } from "@material-ui/icons";
+import { SideBarWithRouter } from "./TemporaryDrawer";
 
 export const Navbar = ({ history }) => {
   const classes = useStyles();
+  const [opened, setOpened] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setOpened(!opened);
+  };
+
   return (
-    <AppBar position="static" color="secondary">
-      <Toolbar variant="dense">
-        <IconButton edge="start" color="inherit">
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="h6" noWrap>
-          Clan-Relations
-        </Typography>
-        <div className={classes.grow} />
-
-        <Tooltip title="Add a Clan">
-          <IconButton
-            color="inherit"
-            onClick={() => history.push("/home/clan")}
-            variant="outlined"
-          >
-            <AddClanIcon />
+    <div>
+      <AppBar position="static" color="secondary">
+        <Toolbar variant="dense">
+          <IconButton edge="start" color="inherit" onClick={handleDrawerToggle}>
+            <MenuIcon />
           </IconButton>
-        </Tooltip>
+          <Typography variant="h6" noWrap>
+            Clan-Relations
+          </Typography>
+          <div className={classes.grow} />
 
-        <Button
-          variant="text"
-          color="inherit"
-          onClick={() => history.push("/clan/hello")}
-        >
-          Show Clans
-        </Button>
+          <Tooltip title="Add a Clan">
+            <IconButton
+              color="inherit"
+              onClick={() => history.push("/home/clan")}
+              variant="outlined"
+            >
+              <AddClanIcon />
+            </IconButton>
+          </Tooltip>
 
-        <Tooltip title="logout">
-          <IconButton
+          {/* <Button
+            variant="text"
             color="inherit"
-            onClick={() => {
-              localStorage.removeItem("token");
-              history.push("/signin");
-            }}
+            onClick={() => history.push("/clan/hello")}
           >
-            <LogoutIcon />
-          </IconButton>
-        </Tooltip>
-      </Toolbar>
-    </AppBar>
+            Show Clans
+          </Button> */}
+
+          <Tooltip title="logout">
+            <IconButton
+              color="inherit"
+              onClick={() => {
+                localStorage.removeItem("token");
+                history.push("/signin");
+              }}
+            >
+              <LogoutIcon />
+            </IconButton>
+          </Tooltip>
+        </Toolbar>
+      </AppBar>
+      <SideBarWithRouter opened={opened} toggleDrawer={handleDrawerToggle} />
+    </div>
   );
 };
