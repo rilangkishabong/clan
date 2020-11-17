a18183e10eec10cf50f184a5cabef03c2909eb50
rilangki <rilangkishabong@gmail.com>
2020-11-17 13:50:11 +0530

    wrapped the app inside ModalProvider for using modals.

--- src/App.js
+++ src/App.js
@@ -8,22 +8,26 @@ import { ThemeProvider } from "@material-ui/core/styles";
 import theme from "./Provider/Theme";
 import { CssBaseline } from "@material-ui/core";
 import AppProvider from "./Provider/AppProvider";
+import { ModalProvider } from "react-modal-hook";
+import { TransitionGroup } from "react-transition-group";
 export const App = () => {
   return (
     <React.StrictMode>
       <CssBaseline />
-      <AppProvider>
-        <ThemeProvider theme={theme}>
-          <BrowserRouter>
-            <Switch>
-              <Route path="/signin" component={SignIn} />
-              <Route path="/signup" component={SignUp} />
-              <AuthorizedRoute path="/" component={Home} />
-              <Redirect to="/signup" />
-            </Switch>
-          </BrowserRouter>
-        </ThemeProvider>
-      </AppProvider>
+      <ModalProvider rootComponent={TransitionGroup}>
+        <AppProvider>
+          <ThemeProvider theme={theme}>
+            <BrowserRouter>
+              <Switch>
+                <Route path="/signin" component={SignIn} />
+                <Route path="/signup" component={SignUp} />
+                <AuthorizedRoute path="/" component={Home} />
+                <Redirect to="/signup" />
+              </Switch>
+            </BrowserRouter>
+          </ThemeProvider>
+        </AppProvider>
+      </ModalProvider>
     </React.StrictMode>
   );
 };
