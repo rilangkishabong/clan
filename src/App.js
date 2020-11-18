cf327e8f30931d562fddde945f59d03459ccdf9c
rilangki <rilangkishabong@gmail.com>
2020-11-18 17:26:46 +0530

    removed appProvider.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import { SignUp } from "./Auth/SignUp";
 import { ThemeProvider } from "@material-ui/core/styles";
 import theme from "./Provider/Theme";
 import { CssBaseline } from "@material-ui/core";
-import AppProvider from "./Provider/AppProvider";
 import { ModalProvider } from "react-modal-hook";
 import { TransitionGroup } from "react-transition-group";
 export const App = () => {
